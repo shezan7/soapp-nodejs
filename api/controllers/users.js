f76a86965475c928ea2077464a6bd5830d742d38
shezan7 <bsse1023@iit.du.ac.bd>
2022-04-26 17:44:39 +0600

    add update and view all user

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -3,6 +3,10 @@ const jwt = require('jsonwebtoken')
 
 const User = require('../sequelize-models/User')
 
+const { Op } = require("sequelize")
+const user = require('../sequelize-models/User')
+
+
 
 exports.users_signup = async (req, res, next) => {
     console.log("users_register", req.body);
@@ -71,7 +75,6 @@ exports.users_login = async (req, res, next) => {
         }
 
         const validPassword = await bcrypt.compare(password, user.password)
-
         if (validPassword) {
             const jwtToken = jwt.sign({
                 id: user.id
@@ -92,6 +95,58 @@ exports.users_login = async (req, res, next) => {
                 message: "Email or Password is incorrect!"
             });
         }
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        })
+    }
+}
+
+exports.users_update = async (req, res, next) => {
+    // console.log(req.body.id)
+    // console.log(req.user.id)
+
+    if (req.user.id === req.body.id) {
+        try {
+            const { id, first_name, last_name, date_of_birth, gender, profile_picture } = req.body;
+
+            const user = await User.update({
+                first_name, last_name, date_of_birth, gender, profile_picture
+            }, {
+                where: {
+                    id
+                }
+            })
+            res.json({
+                message: "User info updated successfully"
+            })
+        } catch (error) {
+            return res.status(500).json(err)
+        }
+    }
+    else {
+        return res.status(403).json("Permision denied")
+    }
+}
+
+exports.users_viewAll = async (req, res, next) => {
+    // console.log(req.user.id)
+    try {
+        const userAll = await User.findAll({
+            attributes: ['id', 'first_name', 'last_name', 'date_of_birth', 'gender', 'profile_picture'],
+            where: {
+                id: {
+                    [Op.ne]: req.user.id
+                }
+            }
+        })
+
+        res.json({
+            data: "All users find successfully",
+            userAll
+        })
     }
     catch (err) {
         console.log(err)
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+const checkAuth = require('../middleware/check-auth');
+
 const UsersController = require('../controllers/users')
 
 
 router.post("/users/register", UsersController.users_signup)
 
 router.post("/users/login", UsersController.users_login)
+
+router.patch("/users/update-info", checkAuth, UsersController.users_update)
+
+router.get("/users/view-all-user", checkAuth, UsersController.users_viewAll)
+
 
 
 
