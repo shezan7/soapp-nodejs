286d956ce59385a6d63f03adf93fa8a54acdd605
Shezan Al-Mahmud <v2.shezan@gmail.com>
2022-05-18 18:23:03 +0600

    chng all updt route to /:id

--- api/routes/post.js
+++ api/routes/post.js
@@ -13,9 +13,9 @@ router.post("/post/create-post", PostController.create_post)
 
 // router.post("/post/create-post", upload.single('image'), PostController.create_post)
 
-router.patch("/post/update-post", PostController.update_post)
+router.patch("/post/update-post/:id", PostController.update_post)
 
-router.delete("/post/delete-post", PostController.delete_post)
+router.delete("/post/delete-post/:id", PostController.delete_post)
 
 router.get("/post/view-post", PostController.view_post)
 
@@ -25,9 +25,9 @@ router.post("/post/unlike", PostController.post_unlike)
 
 router.post("/post/create-comment", PostController.create_comment)
 
-router.patch("/post/update-comment", PostController.update_comment)
+router.patch("/post/update-comment/:id", PostController.update_comment)
 
-router.delete("/post/delete-comment", PostController.delete_comment)
+router.delete("/post/delete-comment/:id", PostController.delete_comment)
 
 
 
